6afc3bb3572f92097dac4c19247bcfa528639d50
zahra.rahimi <zahra.rahimi@gmail.com>
2022-10-01 11:33:13 +0330

    ShoppingCardRepo Methods defined

--- src/main/java/ir/maktab/model/repository/ItemRepo.java
+++ src/main/java/ir/maktab/model/repository/ItemRepo.java
@@ -2,5 +2,9 @@ package ir.maktab.model.repository;
 
 import ir.maktab.model.entity.items.Item;
 
+import java.util.List;
+
 public interface ItemRepo <T extends Item>{
+    List<T> allAvailableItems();    // TODO Can it be a default method?
+
 }
--- src/main/java/ir/maktab/model/repository/ShoppingCardRepo.java
+++ src/main/java/ir/maktab/model/repository/ShoppingCardRepo.java
@@ -1,4 +1,15 @@
 package ir.maktab.model.repository;
 
+import ir.maktab.model.entity.items.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCardRepo {
+    public boolean addItem(){return true;}
+    public boolean deleteItem(){return true;}
+    public List<Item> allItems(){
+        return new ArrayList<>();
+    }
+    public boolean confirmShopping(){return true;}
 }
